fff3f367364b332e8aff0553c2f5a3c9f75b0edc
Josh Russell <jrussell@hubspot.com>
2013-01-24 20:06:23 -0500

    Make option host-name optional

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,15 +1,16 @@
 define dhcp::host (
   $ip,
   $mac,
-  $comment=''
+  $comment='',
+  $option_hostname=$title,
 ) {
 
-  $host = $name
+  $host = $title
   include dhcp::params
 
   $dhcp_dir = $dhcp::params::dhcp_dir
 
-  concat::fragment { "dhcp_host_${name}":
+  concat::fragment { "dhcp_host_${title}":
     target  => "${dhcp_dir}/dhcpd.hosts",
     content => template('dhcp/dhcpd.host.erb'),
     order   => 10,
